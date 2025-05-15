75564e9975b58155c11b7c44239cc3f8d0c38462
DR-Edward <reyesiovanny@gmail.com>
2025-05-15 12:57:06 -0600

    adds RmaRequest Collection

---
+++ Model/ResourceModel/RmaRequest/Collection.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Skuld\OrderReturn\Model\ResourceModel\RmaRequest;
+
+use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
+use Skuld\OrderReturn\Model\RmaRequest;
+use Skuld\OrderReturn\Model\ResourceModel\RmaRequest as RmaRequestResourceModel;
+
+class Collection extends AbstractCollection
+{
+    protected function _construct()
+    {
+        $this->_init(RmaRequest::class, RmaRequestResourceModel::class);
+    }
+}
